78d916e1554dfdd01830411e74163c1a665dbad9
Ubuntu <ubuntu@ip-172-31-55-245.ec2.internal>
2022-04-29 09:25:41 +0000

    using PHP7 error functions

--- exercises/lab9_SQLInjection/SQL_injection2.php
+++ exercises/lab9_SQLInjection/SQL_injection2.php
@@ -5,7 +5,7 @@
     //$c = mysqli_connect("localhost", "root", "password", "users");
     $c = mysqli_connect("localhost", "root", "", "users");
     if(!$c){
- 	echo('Could not connect to db. Contact someone who can help'. mysql_error());
+ 	echo('Could not connect to db. Contact someone who can help'. mysqli_connect_error());
     }
 
     $id = $_GET['id'] or die('<pre> id was not specified</pre>' );
@@ -15,7 +15,7 @@
     $query = "SELECT first_name, last_name FROM t_users WHERE user_id = '".$id."'";
     echo($query);
  
-    $result = mysqli_query($c, $query) or die('<pre>' . mysql_error() . '</pre>' );
+    $result = mysqli_query($c, $query) or die('<pre>' . mysqli_error($c) . '</pre>' );
 
     if ($result && mysqli_num_rows($result) != 0) {
         while($row = mysqli_fetch_assoc($result)) {
