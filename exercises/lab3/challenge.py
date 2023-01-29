ca022a7b98f62579b56c84163c9127155267a92c
mkravchik <kravchik@g.jct.ac.il>
2023-01-29 14:58:28 +0200

    Switched for sha512 and MITRE Attack Groups

--- exercises/lab3/challenge.py
+++ exercises/lab3/challenge.py
@@ -3,8 +3,8 @@ import Crypto.Cipher.AES as AES
 import hashlib
 import binascii
 
-name = b"<A_WELL_KNOWN_CRYPTO_CURRENCY_GOES_HERE>"
-sec = hashlib.sha384((name + b'<REVERSED_ CRYPTO_CURRENCY_GOES_HERE >'  + b'2022')).digest()
+name = b"<A_NAME_OF_SOME_MITRE_THREAT_GROUP_GOES_HERE>"
+sec = hashlib.sha512((name + b'<REVERSED_MITRE_THREAT_GROU_GOES_HERE>' + b'2023')).digest()
 cipher = AES.new(sec[USE_FIRST_32_BYTES], AES.MODE_ECB)
 
 # notice - the message' length is divisible by 16
